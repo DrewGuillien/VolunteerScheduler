02b77ad0ac7685894f20579fda6cc621b59bfb28
guillien.drew <guillien.drew@uwlax.edu>
2017-10-09 01:59:34 -0500

    Activities and programs pages started and stubbed a few routes

--- VolunteerScheduler/routes/index.js
+++ VolunteerScheduler/routes/index.js
@@ -4,8 +4,48 @@ var router = express.Router();
 // var users = require('./users');
 
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.sendFile('index.html', { root: __dirname + "/public" });
+router.get('/', function (request, response) {
+    response.sendFile('index.html', { root: __dirname + "/public" });
+});
+
+router.get("/programs", function (request, response) {
+
+});
+
+router.get("/programs/:programId", function (request, response) {
+
+});
+
+router.post("/programs", function (request, response) {
+
+});
+
+router.put("/programs", function (request, response) {
+
+});
+
+router.delete("/programs", function (request, response) {
+
+});
+
+router.get("/programs/:programId/activities", function (request, response) {
+    
+});
+
+router.get("/programs/:programId/activities/:activityId", function (request, response) {
+
+});
+
+router.post("/programs/:programId/activities", function (request, response) {
+
+});
+
+router.put("/programs/:programId/activities", function (request, response) {
+
+});
+
+router.delete("/programs/:programId/activities", function (request, response) {
+
 });
 
 module.exports = router;
---
+++ VolunteerScheduler/views/shared/pages/activitiesController.js
@@ -0,0 +1,15 @@
+﻿activities = angular.module("Volunteer.Activities", ["$ngRoute"]);
+
+activities.config(["$routeProvider", function ($routeProvider) {
+    var config = {
+        templateUrl: "view/shared/pages/activities.html",
+        controller: "Volunteer.Activities"
+    }
+    $routeProvider.when("/activities", config);
+}]);
+
+activities.controller("Volunteer.Activities.Controller", ["$scope", "$http", "$routeParams"], function ($scope, $http, $routeParams) {
+    $http.get("/activities").then(function(response) {
+        $scope.activities = response;
+    });
+});
