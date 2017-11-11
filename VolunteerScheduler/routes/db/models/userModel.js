2e0c143849b8723195237f491eec872dec7e5fee
guillien.drew <guillien.drew@uwlax.edu>
2017-11-11 17:30:17 -0600

    renamed and moved files

--- VolunteerScheduler/routes/db/models/userModel.js
+++ VolunteerScheduler/routes/db/models/userModel.js
@@ -1,15 +1,52 @@
-﻿var mongoose = require("mongoose");
+﻿var bcrypt = require("bcrypt");
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var userSchema = Schema({
+var userSchema = new Schema({
     role: [String],
-    username: String,
-    password: String, //encrypted string
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String, //encrypted by bcrypt
+        required: true
+    },
     name: { first: String, last: String },
     email: String,
 
     enabled: Boolean
 });
+
+userSchema.pre("save", function (next) {
+    var user = this;
+    if (user.isModified("password") || user.isNew) {
+        bcrypt.genSalt(10, function (error, salt) {
+            if (error) {
+                return next(error);
+            }
+            bcrypt.hash(user.password, salt, function (error, hash) {
+                if (error) {
+                    return next(error);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+userSchema.methods.comparePassword = function (password, callback) {
+    bcrypt.compare(password, this.password, function (error, isMatch) {
+        if (error) {
+            return callback(error);
+        }
+        callback(null, isMatch);
+    });
+};
 
 userSchema.set('toJSON',  {
     transform: function (doc, result, options) {
